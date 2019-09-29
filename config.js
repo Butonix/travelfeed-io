8427e15017ad0d91324f5cb5773e4006010a0dd4
tiotdev <tiotdev@tiot.de>
2019-09-30 01:57:26 +0200

    Version bump to 2.0.0 and minor dependency updates

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ export const STEEM_API = process.env.STEEM_API;
 
 export const STEEMCONNECT_CALLBACK_URL = `${ROOTURL}/login`;
 
-export const APP_VERSION = 'travelfeed/1.5.0';
+export const APP_VERSION = 'travelfeed/2.0.0';
 export const DEFAULT_META_DESCRIPTION =
   'Find inspiration for your travels on TravelFeed. Join the TravelFeed community, write your own travel blog and start earning!';
