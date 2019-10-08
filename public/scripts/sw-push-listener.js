2975b4b61a3ab5445435de32a9630f3277977546
tiotdev <tiotdev@tiot.de>
2019-10-09 00:54:56 +0700

    Handle notificationclick for web push

--- public/scripts/sw-push-listener.js
+++ public/scripts/sw-push-listener.js
@@ -4,13 +4,14 @@
 self.addEventListener('push', function(event) {
   if (event.data) {
     var data = event.data.json();
-    var { body, title, actions, icon, badge, image } = data;
+    var { body, title, actions, icon, badge, image, data } = data;
     var notificationOptions = { body };
 
     if (actions) notificationOptions.actions = actions;
     if (image) notificationOptions.image = image;
     if (icon) notificationOptions.icon = icon;
     if (badge) notificationOptions.badge = badge;
+    if (data) notificationOptions.data = data;
 
     event.waitUntil(
       Promise.all([
@@ -20,3 +21,17 @@ self.addEventListener('push', function(event) {
     );
   }
 });
+
+self.addEventListener(
+  'notificationclick',
+  function(event) {
+    var data = event.notification.data;
+
+    event.notification.close();
+
+    if (event.action === 'open-post') {
+      clients.openWindow('/@' + data.author + '/' + data.permlink);
+    }
+  },
+  false,
+);
