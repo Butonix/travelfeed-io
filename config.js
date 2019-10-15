f75f5a27862602ba8bac7e46455f058323f44df6
tiotdev <tiotdev@tiot.de>
2019-10-15 16:04:39 +0700

    Version bump, get APP_VERSION from package.json

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 // Do not use destructuring!
 // https://github.com/zeit/next.js/tree/master#build-time-configuration
 
+const { version } = require('./package.json');
+
 export const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 export const GMAPS_API_KEY = process.env.GMAPS_API_KEY;
 export const RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY;
@@ -12,7 +14,7 @@ export const STEEM_API = process.env.STEEM_API;
 
 export const STEEMCONNECT_CALLBACK_URL = `${ROOTURL}/login`;
 
-export const APP_VERSION = 'travelfeed/2.0.0';
+export const APP_VERSION = `travelfeed/${version}`;
 export const DEFAULT_META_DESCRIPTION =
   'Find inspiration for your travels on TravelFeed. Join the TravelFeed community, write your own travel blog and start earning!';
 
