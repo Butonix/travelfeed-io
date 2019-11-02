1ab43e190ed1744d5c2f838ddcbb265469fb0944
tiotdev <tiotdev@tiot.de>
2019-11-02 23:58:54 +0800

    Improve referred users loading

--- components/Dashboard/Contest/Referrals.js
+++ components/Dashboard/Contest/Referrals.js
@@ -15,56 +15,57 @@ const Referrals = () => {
     <>
       <Query query={REFERRALS}>
         {({ data }) => {
-          if (data && data.referrals) {
-            let count = 0;
-            data.referrals.forEach(c => {
-              count += 1;
-            });
-            return (
-              <HeaderCard
-                title={
-                  count
-                    ? `Your have Referred ${count} User${count > 1 ? 's' : ''}`
-                    : "You haven't Referred Anyone yet"
-                }
-                background={cyan[600]}
-                content={
-                  <>
-                    {(data.referrals && data.referrals.length > 0 && (
-                      <div
-                        style={{
-                          overflowX: 'auto',
-                          wordWrap: 'normal',
-                          wordBreak: 'normal',
-                        }}
-                      >
-                        <Table>
-                          <TableHead>
-                            <TableRow>
-                              <TableCell>Username</TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {data.referrals.map((c, i) => (
-                              <TableRow hover key={i}>
+          let title = '';
+          if (data && data.referrals && data.referrals.length > 0)
+            title = `Your have Referred ${data.referrals.length} User${
+              data.referrals.length > 1 ? 's' : ''
+            }`;
+          else if (data && data.referrals && data.referrals.length === 0)
+            title = "You haven't Referred Anyone yet";
+          return (
+            <HeaderCard
+              title={title}
+              background={cyan[600]}
+              content={
+                <>
+                  {(data && data.referrals && data.referrals.length > 0 && (
+                    <div
+                      style={{
+                        overflowX: 'auto',
+                        wordWrap: 'normal',
+                        wordBreak: 'normal',
+                      }}
+                    >
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Username</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {data &&
+                            data.referrals &&
+                            data.referrals.length > 0 &&
+                            data.referrals.map(c => (
+                              <TableRow hover key={c}>
                                 <TableCell>{c}</TableCell>
                               </TableRow>
                             ))}
-                          </TableBody>
-                        </Table>
-                      </div>
-                    )) || (
+                        </TableBody>
+                      </Table>
+                    </div>
+                  )) ||
+                    ((data && data.referrals && data.referrals.length === 0 && (
                       <Typography variant="body1">
                         Once your referrals join TravelFeed, they will show up
                         here.
                       </Typography>
-                    )}
-                  </>
-                }
-              />
-            );
-          }
-          return 'Loading...';
+                    )) ||
+                      '')}
+                </>
+              }
+            />
+          );
         }}
       </Query>
     </>
